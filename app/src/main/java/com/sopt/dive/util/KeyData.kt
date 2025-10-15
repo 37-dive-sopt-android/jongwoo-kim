76070a14b5f081a41915d76a767a66bf7592c74f
김종우 <kimjw111003@gmail.com>
2025-10-15 16:07:37 +0900

    Revert "[feat] 필수 과제"
    This reverts commit 191f7963be8444d84f4706d45becadf52b6badaf.

--- app/src/main/java/com/sopt/dive/util/KeyData.kt
+++
@@ -1,8 +0,0 @@
-package com.sopt.dive.util
-
-object PrefsConst {
-    const val ID_DATA = "idData"
-    const val PW_DATA = "pwData"
-    const val NICKNAME_DATA = "nickNameData"
-    const val DRINK_DATA = "drinkData"
-}
