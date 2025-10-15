9c0959a5393c855f295903f760963238c8829a36
김종우 <kimjw111003@gmail.com>
2025-10-15 09:44:38 +0900

    [feat] 심화 과제
    (cherry picked from commit 0a0282439e980edb25839b8e52f1c5abd1a2b5f9)

--- app/src/main/java/com/sopt/dive/login/LoginActivity.kt
+++ app/src/main/java/com/sopt/dive/login/LoginActivity.kt
@@ -14,8 +14,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -34,8 +37,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -47,6 +53,7 @@ import androidx.lifecycle.lifecycleScope
 import com.sopt.dive.MainActivity
 import com.sopt.dive.R
 import com.sopt.dive.signup.SignUpActivity
+import com.sopt.dive.util.noRippleClickable
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -85,6 +92,8 @@ class LoginActivity : ComponentActivity() {
         modifier: Modifier = Modifier,
         loginSuccessCallback: () -> Unit
     ) {
+        val focusManager = LocalFocusManager.current
+
         var idText by remember { mutableStateOf("") }
         var pwText by remember { mutableStateOf("") }
 
@@ -98,113 +107,136 @@ class LoginActivity : ComponentActivity() {
         }
 
 
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(vertical = 40.dp, horizontal = 16.dp)
-        ) {
-            Text(
-                text = stringResource(R.string.login_title),
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
-            )
-
-            Spacer(Modifier.weight(0.2f))
-
-            /** ID **/
-            Text(
-                text = stringResource(R.string.login_id_title),
-                fontSize = 32.sp,
-                fontWeight = FontWeight.Normal,
-                color = Color.Black
-            )
-
-            TextField(
-                value = idText,
-                onValueChange = { idText = it },
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.Transparent,
-                    unfocusedContainerColor = Color.Transparent,
-                    unfocusedPlaceholderColor = Color.LightGray
-                ),
-                placeholder = {
+        Scaffold(
+            bottomBar = {
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .imePadding()
+                        .navigationBarsPadding()
+                        .fillMaxWidth()
+                ) {
                     Text(
-                        text = stringResource(R.string.login_id_hint)
+                        text = stringResource(R.string.login_signup_button),
+                        fontSize = 12.sp,
+                        color = Color.Black,
+                        textDecoration = TextDecoration.Underline,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .noRippleClickable {
+                                val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+                                signupActivityResult.launch(intent)
+                            }
                     )
-                },
-                modifier = Modifier.fillMaxWidth()
-            )
 
-            Spacer(Modifier.height(16.dp))
+                    Spacer(Modifier.height(8.dp))
 
-            /** PW **/
-            Text(
-                text = stringResource(R.string.login_pw_title),
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Normal,
-                color = Color.Black
-            )
+                    Button(
+                        onClick = {
+                            if(idText == answerId && pwText == answerPw) {
+                                loginSuccessCallback()
+                            } else {
 
-            TextField(
-                value = pwText,
-                onValueChange = { pwText = it },
-                visualTransformation = PasswordVisualTransformation(),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.Transparent,
-                    unfocusedContainerColor = Color.Transparent,
-                    unfocusedPlaceholderColor = Color.LightGray
-                ),
-                placeholder = {
-                    Text(
-                        text = stringResource(R.string.login_pw_hint)
-                    )
-                },
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            Spacer(Modifier.weight(1f))
-
-            Text(
-                text = stringResource(R.string.login_signup_button),
-                fontSize = 12.sp,
-                color = Color.Black,
-                textDecoration = TextDecoration.Underline,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .clickable {
-                        val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
-                        signupActivityResult.launch(intent)
-                    }
-            )
-
-            Spacer(Modifier.height(8.dp))
-
-            Button(
-                onClick = {
-                    if(idText == answerId && pwText == answerPw) {
-                        loginSuccessCallback()
-                    } else {
-
-                    }
-                },
-                modifier = Modifier.Companion
-                    .fillMaxWidth()
-                    .height(40.dp),
-                shape = RoundedCornerShape(24.dp),
-                colors = ButtonColors(
-                    containerColor = Color.Blue,
-                    contentColor = Color.Black,
-                    disabledContainerColor = Color.Gray,
-                    disabledContentColor = Color.White
-                ),
-                content = {
-                    Text(
-                        text = stringResource(R.string.login_button),
-                        color = Color.White
+                            }
+                        },
+                        modifier = Modifier.Companion
+                            .fillMaxWidth()
+                            .height(40.dp),
+                        shape = RoundedCornerShape(24.dp),
+                        colors = ButtonColors(
+                            containerColor = Color.Blue,
+                            contentColor = Color.Black,
+                            disabledContainerColor = Color.Gray,
+                            disabledContentColor = Color.White
+                        ),
+                        content = {
+                            Text(
+                                text = stringResource(R.string.login_button),
+                                color = Color.White
+                            )
+                        }
                     )
                 }
-            )
+            }
+        ) { paddingValues ->
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.login_title),
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+
+                Spacer(Modifier.weight(0.2f))
+
+                /** ID **/
+                Text(
+                    text = stringResource(R.string.login_id_title),
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.Black
+                )
+
+                TextField(
+                    value = idText,
+                    onValueChange = { idText = it },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                        unfocusedPlaceholderColor = Color.LightGray
+                    ),
+                    singleLine = true,
+                    keyboardActions = KeyboardActions(onNext = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    }),
+                    placeholder = {
+                        Text(
+                            text = stringResource(R.string.login_id_hint)
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(Modifier.height(16.dp))
+
+                /** PW **/
+                Text(
+                    text = stringResource(R.string.login_pw_title),
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.Black
+                )
+
+                TextField(
+                    value = pwText,
+                    onValueChange = { pwText = it },
+                    visualTransformation = PasswordVisualTransformation(),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                        unfocusedPlaceholderColor = Color.LightGray
+                    ),
+                    singleLine = true,
+                    keyboardActions = KeyboardActions(onDone = {
+                        focusManager.clearFocus()
+                    }),
+                    placeholder = {
+                        Text(
+                            text = stringResource(R.string.login_pw_hint)
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(Modifier.weight(1f))
+
+
+            }
         }
     }
 
